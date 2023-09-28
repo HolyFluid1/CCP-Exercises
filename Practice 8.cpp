4f4ab7da3d9103da7acc8135facdd399cddcd2d0
Erick James Barrientos <146304162+HolyFluid1@users.noreply.github.com>
2023-09-28 10:53:09 +0800

    Add files via upload

---
+++ Practice 8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	//practice 8
+	//Gwapo AKo
+	string Jimwell;
+	
+	cout <<"Enter your name:";
+	getline(cin,Jimwell); //para mag boue it input
+	cout<<"The lenght of your name is: "<<Jimwell.size();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
